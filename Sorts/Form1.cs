37274b88f862599d96de374d4f4e4b840025825c
bjiake <suyd@list.ru>
2022-11-28 19:54:44 +0700

    чек

--- Sorts/Form1.cs
+++ Sorts/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,6 +23,11 @@ namespace Sorts {
             table.Columns.Add("index", typeof(int));
             table.Columns.Add("value", typeof(double));
             dataGridView1.Rows[0].Cells[0].Value = 0;
+            chartBubble.Titles[0].Text = "Bubble";
+            chartBogo.Titles[0].Text = "BOGO";
+            chartFast.Titles[0].Text = "Fast";
+            chartInputs.Titles[0].Text = "Вставки";
+            chartShake.Titles[0].Text = "Shake";
         }
 
         List<double> items = new List<double>();
@@ -30,80 +36,59 @@ namespace Sorts {
             int index = 0;
             foreach (DataGridViewRow currRow in dataGridView1.Rows) {
                 items.Add(Convert.ToDouble(currRow.Cells[1].Value));
-                chart1.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
-                    index++;
+                chartBubble.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
+                chartBogo.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
+                chartFast.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
+                chartInputs.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
+                chartShake.Series[0].Points.AddXY(index, Convert.ToDouble(currRow.Cells[1].Value));
+                index++;
             }
             items.Remove(items.Last());
         }
-
-       
-
         private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e) {
             dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = dataGridView1.Rows.Count - 1;
         }
 
-        public struct PointD {
-            public double X;
-            public double Y;
+        private async void btSort_Click(object sender, EventArgs e) {
+            chartBubble.Titles[0].Text = "Bubble";
+            chartBogo.Titles[0].Text = "BOGO";
+            chartFast.Titles[0].Text = "Fast";
+            chartInputs.Titles[0].Text = "Вставки";
+            chartShake.Titles[0].Text = "Shake";
 
-            public PointD(double x, double y) {
-                X = x;
-                Y = y;
-            }
+            double[] array = new double[items.Count];
+            array = addArray(array);
+            Parallel.Invoke(
+                () => {
+                    if (cbBubble.Checked) {
+                        //await Task.Run(() => { DrawBubbleSortAsync(array); });
+                        DrawBubbleSortAsync(array);
+                        //DrawBubbleSortAsync(array);
+                    }
+                }, () => {
+                    if (cbInsert.Checked) {
+                        //await Task.Run(() => { SortInputs(array); });
+                        SortInputs(array);
+                        //SortInputs(array);
+                    }
+                }, () => {
+                    if (cbFast.Checked) {
+                        //await Task.Run(() => { FastSort(array); return Task.CompletedTask; });
+                        //FastSort(array);
+                        //FastSort(array);
+                    }
+                }, () => {
+                    if (cbShake.Checked) {
+                        //await Task.Run(() => { ShakeSort(array); });
+                        ShakeSort(array);
+                    }
+                }, () => {
 
-            public Point ToPoint() {
-                return new Point((int)X, (int)Y);
-            }
-
-            public override bool Equals(object obj) {
-                return obj is PointD && this == (PointD)obj;
-            }
-            public override int GetHashCode() {
-                return X.GetHashCode() ^ Y.GetHashCode();
-            }
-            public static bool operator ==(PointD a, PointD b) {
-                return a.X == b.X && a.Y == b.Y;
-            }
-            public static bool operator !=(PointD a, PointD b) {
-                return !( a == b );
-            }
-        }
-
-        private void btSort_Click(object sender, EventArgs e) {
-            if (cbBubble.Checked) {
-                chart1.Titles[0].Text = "Bubble";
-
-                //Секундомер
-                Time timeBubble = new Time();
-                timeBubble.TimeStart();
-                string resultTime = String.Empty;
-
-                //Сама сортировка
-                SortBubble();
-                resultTime = timeBubble.TimeEnd();
-
-                //Визуализация сортировки
-                DrawBubbleSortAsync();
-                //PointD tempPoint = new PointD();
-                //tempPoint.Y = chart1.Series[0].Points[0].YValues[0];
-
-                //Thread.Sleep(1000);
-                //chart1.Series[0].Points[0].YValues[0] = chart1.Series[0].Points[1].YValues[0];
-
-                //Thread.Sleep(1000);
-                //chart1.Series[0].Points[1].YValues[0] = tempPoint.Y;
-
-                //Остановка секундомера
-                lbTimer.Text = resultTime;
-            } else if (cbInsert.Checked) {
-
-            } else if (cbFast.Checked) {
-
-            } else if (cbShake.Checked) { 
-
-            } else if (cbBogo.Checked) {
-
-            }
+                    if (cbBogo.Checked) {
+                        //await Task.Run(() => { BogoSort(array); });
+                        BogoSort(array);
+                    }
+                });
         }
         private double[] addArray(double[] array) {
             int index = 0;
@@ -113,65 +98,288 @@ namespace Sorts {
             }
             return array;
         }
-
-
-
-        private void DrawBubbleSortAsync() {
-            double temp;
-            PointD tempPoint = new PointD();
-
-
-            double[] array = new double[items.Count];
-            array = addArray(array);
-
-
-            for (int i = 0; i < array.Length; i++) {
-                for (int j = i + 1; j < array.Length; j++) {
-                    if (array[i] > array[j]) {
-
-
-                        temp = array[i];
-                        tempPoint.Y = chart1.Series[0].Points[i].YValues[0];
-
-                        Thread thread1 = new Thread(() => {
-                            array[i] = array[j];
-                            chart1.Series[0].Points[i].YValues[0] = chart1.Series[0].Points[j].YValues[0];
-                        });
-                        thread1.Start();
-                        
-                        
-                        Thread thread2 = new Thread(() => {
-                            array[j] = temp;
-                            chart1.Series[0].Points[j].YValues[0] = tempPoint.Y;
-                            Thread.Sleep(1000);
-                        });
-                        thread2.Start();
-                        
-                    }
-                }
-            }
-
-
+        private void DrawTimeResult(Chart c, string text) {
+            c.Invoke(new Action(() => c.Titles[0].Text += " " + text + "мс"));
         }
-        #region Сортировка Пузырьком
-        private double[] SortBubble() {
-            double temp;
+        #region Сортировки
 
-            double[] array = new double[items.Count];
-            array = addArray(array);
+        #region Сортировка Пузырьком
+
+        private double[] DrawBubbleSortAsync(double[] array) {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            double temp;
 
             for (int i = 0; i < array.Length; i++) {
                 for (int j = i + 1; j < array.Length; j++) {
                     if (array[i] > array[j]) {
+
+
                         temp = array[i];
                         array[i] = array[j];
                         array[j] = temp;
+                        if (cbVisual.Checked) {
+                            sw.Stop();
+                            //Invoke(new Action(() => Visualization(chartBubble, array)));
+                            Visualization(chartBubble, array);
+                            sw.Start();
+                        }
+
                     }
                 }
             }
+            Wait(1);
+            sw.Stop();
+            DrawTimeResult(chartBubble, sw.ElapsedMilliseconds.ToString());
+            //chartBubble.Titles[0].Text += " " + sw.ElapsedMilliseconds.ToString() + "мс";
+
             return array;
-            
         }
         #endregion
+        #region Сортировка вставками
+        private double[] SortInputs(double[] array) {
+            Stopwatch SW = new Stopwatch();
+            SW.Start();
+
+
+            for (int i = 1; i < array.Length; i++) {
+                double k = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > k) {
+                    array[j + 1] = array[j];
+                    array[j] = k;
+                    j--;
+                    if (cbVisual.Checked) {
+                        SW.Stop();
+                        //Invoke(new Action(() => Visualization(chartInputs, array)));
+                        Visualization(chartInputs, array);
+                        SW.Start();
+                    }
+                }
+            }
+            Wait(1);
+            SW.Stop();
+            DrawTimeResult(chartInputs, SW.ElapsedMilliseconds.ToString());
+
+            //chartInputs.Titles[0].Text += " " + SW.ElapsedMilliseconds.ToString() + "мс";
+            return array;
+        }
+        #endregion
+
+        #region shake
+        private double[] ShakeSort(double[] Array) {
+            Stopwatch SW = new Stopwatch();
+            
+            SW.Start();
+            int left = 0,
+            right = Array.Length - 1;
+            while (left < right) {
+                for (int i = left; i < right; i++) {
+                    if (Array[i] > Array[i + 1]) {
+                        SW.Stop();
+                        Swap(chartShake, Array, i, i + 1);
+                        SW.Start();
+                        if (cbVisual.Checked) {
+                            SW.Stop();
+                            Invoke(new Action(() => Visualization(chartShake, Array)));
+                            SW.Start();
+                        }
+                    }
+                }
+                right--;
+                for (int i = right; i > left; i--) {
+                    if (Array[i - 1] > Array[i]) {
+                        SW.Stop();
+                        Swap(chartShake, Array, i - 1, i);
+                        SW.Start();
+                        if (cbVisual.Checked) {
+                            SW.Stop();
+                            Invoke(new Action(() => Visualization(chartShake, Array)));
+                            SW.Start();
+                        }
+                    }
+                }
+                left++;
+            }
+            Wait(1);
+            SW.Stop();
+            DrawTimeResult(chartShake, SW.ElapsedMilliseconds.ToString());
+            //chartShake.Titles[0].Text += " " + SW.ElapsedMilliseconds.ToString() + "мс";
+            return Array;
+        }
+        #endregion
+
+        #region Fast
+        private double[] FastSort(double[] Array) {
+
+            Stopwatch SW = new Stopwatch();
+            SW.Start();
+            var stack = new Stack<int>();
+
+            double pivot;
+            int pivotIndex = 0;
+            int leftIndex = pivotIndex + 1;
+            int rightIndex = Array.Length - 1;
+
+            stack.Push(pivotIndex);
+            stack.Push(rightIndex);
+
+            int leftIndexOfSubSet, rightIndexOfSubset;
+
+            while (stack.Count > 0) {
+                if (cbVisual.Checked) {
+                    SW.Stop();
+                    Invoke(new Action(() => Visualization(chartFast, Array)));
+                   // Visualization(chartFast, Array);
+                    SW.Start();
+                }
+                rightIndexOfSubset = stack.Pop();
+                leftIndexOfSubSet = stack.Pop();
+
+                leftIndex = leftIndexOfSubSet + 1;
+                pivotIndex = leftIndexOfSubSet;
+                rightIndex = rightIndexOfSubset;
+
+                pivot = Array[pivotIndex];
+
+                if (leftIndex > rightIndex) {
+                    continue;
+                }
+
+                while (leftIndex < rightIndex) {
+                    while (( leftIndex <= rightIndex ) && ( Array[leftIndex] <= pivot )) {
+                        leftIndex++;
+                    }
+
+                    while (( leftIndex <= rightIndex ) && ( Array[rightIndex] >= pivot )) {
+                        rightIndex--;
+                    }
+
+                    if (rightIndex >= leftIndex) {
+                        SW.Stop();
+                        Swap(chartFast, Array, leftIndex, rightIndex);
+                        SW.Start();
+                    }
+                }
+
+                if (pivotIndex <= rightIndex) {
+                    if (Array[pivotIndex] > Array[rightIndex]) {
+                        SW.Stop();
+                        Swap(chartFast, Array, pivotIndex, rightIndex);
+                        SW.Start();
+                    }
+                }
+
+                if (leftIndexOfSubSet < rightIndex) {
+                    stack.Push(leftIndexOfSubSet);
+                    stack.Push(rightIndex - 1);
+                }
+
+                if (rightIndexOfSubset > rightIndex) {
+                    stack.Push(rightIndex + 1);
+                    stack.Push(rightIndexOfSubset);
+                }
+            }
+            Wait(1);
+            SW.Stop();
+            DrawTimeResult(chartFast, SW.ElapsedMilliseconds.ToString());
+
+            return Array;
+            //chartFast.Titles[0].Text += " " + SW.ElapsedMilliseconds.ToString() + "мс";
+        }
+        #endregion
+        #region Bogo
+        public void BogoSort(double[] Array) {
+            double Temp;
+            int Rnd;
+            Stopwatch SW = new Stopwatch();
+            Random Random = new Random();
+            SW.Start();
+            while (!IsSorted(Array)) {
+                for (int i = 0; i < Array.Length; ++i) {
+                    Rnd = Random.Next(Array.Length);
+                    Temp = Array[i];
+                    Array[i] = Array[(int)Rnd];
+                    Array[(int)Rnd] = Temp;
+                    if (cbVisual.Checked) {
+                        Invoke(new Action(() => Visualization(chartBogo, Array)));
+                    }
+                }
+            }
+            Wait(1);
+            SW.Stop();
+            DrawTimeResult(chartBogo, SW.ElapsedMilliseconds.ToString());
+            //chartBogo.Titles[0].Text += " " + SW.ElapsedMilliseconds.ToString() + "мс";
+        }
+        #endregion
+        #endregion
+
+        private bool IsSorted(double[] Array) {
+            int Count = Array.Length;
+            while (--Count >= 1) {
+                if (Array[Count] < Array[Count - 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public void Visualization(Chart c, double[] Array) {
+            c.Series[0].Points.Clear();
+
+            for (int i = 0; i < Array.Length; i++) {
+                c.Series[0].Points.AddY(Array[i]);
+            }
+            Wait(0.001);
+        }
+
+        private void Wait(double seconds) {
+            int ticks = System.Environment.TickCount + (int)Math.Round(seconds * 1000.0);
+            while (System.Environment.TickCount < ticks) {
+                Application.DoEvents();
+            }
+        }
+        private void Swap(Chart c, double[] array, int i, int j) {
+            double Temp = array[i];
+            array[i] = array[j];
+            array[j] = Temp;
+            if (cbVisual.Checked) {
+                Invoke(new Action(() => Visualization(c, array)));
+            }
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e) {
+            items.Clear();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Rows[0].Cells[0].Value = 0;
+            try {
+                chartBubble.Series[0].Points.Clear();
+                chartBogo.Series[0].Points.Clear();
+                chartFast.Series[0].Points.Clear();
+                chartInputs.Series[0].Points.Clear();
+                chartShake.Series[0].Points.Clear();
+            } catch (Exception) {
+
+            }
+
+        }
+        bool isFirstClick = true;
+
+        private void randomToolStripMenuItem_Click(object sender, EventArgs e) {
+            if (isFirstClick) { isFirstClick = false; } else {
+                ClearToolStripMenuItem_Click(sender, e);
+            }
+
+
+            Random Random = new Random();
+            double[] RandomArray = new double[100];
+            for (int i = 0; i < RandomArray.Length; i++) {
+                RandomArray[i] = Random.Next(1, 10);
+                dataGridView1.Rows.Add(i, RandomArray[i]);
+            }
+
+            //btDataAdd_Click(sender, e);
+        }
     }
 }
